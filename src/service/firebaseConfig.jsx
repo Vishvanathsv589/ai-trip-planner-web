71e61cc4b984f3f40b1d4ef3405e700aa3f7d206
Vishvanath589 <vishvanathsv04@gmail.com>
2025-03-13 23:09:39 +0530

    API UPDATE

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -8,7 +8,7 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCaTui0hR3Fw08WhHMjFw6xNXG9HXk-QT0",
+  apiKey: FIREBASE_AUTH_ID,
   authDomain: "ai-trip-a2321.firebaseapp.com",
   projectId: "ai-trip-a2321",
   storageBucket: "ai-trip-a2321.firebasestorage.app",
