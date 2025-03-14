28d10c7ab20a8aa70e0d91a570f2bcb0086e6a8c
Vishvanath589 <vishvanathsv04@gmail.com>
2025-03-14 09:03:28 +0530

    4

--- src/view-trip/[tripId]/components/InfoSection.jsx
+++ src/view-trip/[tripId]/components/InfoSection.jsx
@@ -15,9 +15,9 @@ function InfoSection({ trip }) {
             textQuery:trip?.userSelection?.location?.label
         }
         const result=await GetPlaceDetails(data).then(resp=>{
-            console.log(resp.data.places[0].photos[1].name);
+            console.log(resp.data.places[0].photos[3].name);
 
-            const PhotoUrl=PHOTO_REF_URL.replace('{NAME}',resp.data.places[0].photos[4].name);
+            const PhotoUrl=PHOTO_REF_URL.replace('{NAME}',resp.data.places[0].photos[2].name);
             setPhotoUrl(PhotoUrl);
         })
     }
