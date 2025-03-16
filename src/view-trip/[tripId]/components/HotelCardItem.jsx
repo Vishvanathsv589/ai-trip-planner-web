9917895b9ac964772ecf61d6781165a8b7cc3fa1
Vishvanath589 <vishvanathsv04@gmail.com>
2025-03-16 21:04:33 +0530

    1

--- src/view-trip/[tripId]/components/HotelCardItem.jsx
+++ src/view-trip/[tripId]/components/HotelCardItem.jsx
@@ -15,7 +15,7 @@ function HotelCardItem({ hotel }) {
         const result = await GetPlaceDetails(data).then(resp => {
             console.log(resp.data.places[0].photos[3].name);
 
-            const PhotoUrl = PHOTO_REF_URL.replace('{NAME}', resp.data.places[0].photos[3].name);
+            const PhotoUrl = PHOTO_REF_URL.replace('{NAME}', resp.data.places[0].photos[0].name);
             setPhotoUrl(PhotoUrl);
         })
     }
